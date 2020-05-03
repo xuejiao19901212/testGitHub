732ff4e6d215abf64b22f54283e5f25492cc9b00
chao02.yang <chao02.yang@horizon.ai>
2020-05-03 20:58:41 +0800

    edit by yangchao 5.3

--- src/main/java/com/atguigu/oss/test/OSSTest.java
+++ src/main/java/com/atguigu/oss/test/OSSTest.java
@@ -10,7 +10,7 @@ public class OSSTest {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		// Endpoint以杭州为例，其它Region请按实际情况填写。
-		String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";     //阿里云服务
+		String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";     //阿里云服务EDIT BY YANGCHAO
 		
 		// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
 		String accessKeyId = "LTAIRyb77bOaduym";
